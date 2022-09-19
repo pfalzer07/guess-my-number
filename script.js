cba20e3cb4677c942139014b090f11ab6768a08f
Ahmed <ahmedlekin.97@gmail.com>
2022-09-19 23:20:01 +0300

    final state of the game

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess, guess);
+
+  //When no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+
+    //When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number!';
+    document.querySelector('body').style.backgroundColor = `#60b347`;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+    //When guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+    } else {
+      document.querySelector('.message').textContent = 'Game Over';
+    }
+    document.querySelector('.score').textContent = score;
+
+    //When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game Over';
+    }
+  }
+});
+
+//Implementing Again button
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = `#222`;
+});
