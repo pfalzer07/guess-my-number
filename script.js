441deb9c6c5e80dfac1752c566430ad8dd6732b8
Ahmed <ahmedlekin.97@gmail.com>
2022-09-19 23:29:53 +0300

    some refactoring

--- script.js
+++ script.js
@@ -23,23 +23,15 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.highscore').textContent = highscore;
     }
     //When guess is too high
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high!';
-      score--;
-    } else {
-      document.querySelector('.message').textContent = 'Game Over';
-    }
-    document.querySelector('.score').textContent = score;
-
-    //When guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low!';
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too high' : 'Too low';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = 'Game Over';
+      document.querySelector('.score').textContent = 0;
     }
   }
 });
